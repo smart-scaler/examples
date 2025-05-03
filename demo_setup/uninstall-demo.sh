69267c2fb8751d50e79665d5aa4c1cf7cad7bd3d
Dheeraj Ravula <105057632+Dheeraj-k8s@users.noreply.github.com>
2025-05-03 16:49:37 -0400

    Update uninstall-demo.sh
    removing istio and keda

--- demo_setup/uninstall-demo.sh
+++ demo_setup/uninstall-demo.sh
@@ -1,6 +1,6 @@
 helm uninstall acme
 helm uninstall boutique
-helm uninstall keda
+#helm uninstall keda
 kubectl delete -f ./istio-1.19.3/samples/addons/grafana.yaml
 kubectl delete -f ./istio-1.19.3/samples/addons/prometheus.yaml
-./istio-1.19.3/bin/istioctl uninstall --skip-confirmation --revision default
+#./istio-1.19.3/bin/istioctl uninstall --skip-confirmation --revision default
